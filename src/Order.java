67414a4d78e1f8393bf24829ad49ec8c48d2f2eb
menardmalo <malo.menard02@gmail.com>
2023-06-07 15:58:46 -0400

    first commit from master

--- src/Order.java
+++ src/Order.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Order {
+
+    public void attach(OrderObserver observer) {
+    }
+
+    public void detach(OrderObserver observer) {
+    }
+
+    public double getTotalPrice() {
+    }
+
+    public int getCount() {
+    }
+
+    public String toString() {
+        return null
+    }
 }
