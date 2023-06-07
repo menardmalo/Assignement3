d4394778ddb576a4822ff85db4b937c64db7e573
menardmalo <malo.menard02@gmail.com>
2023-06-07 16:07:54 -0400

    Add all the code

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,25 @@
 public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        Order order = new Order();
+        PriceObserver priceObserver = new PriceObserver();
+        QuantityObserver quantityObserver = new QuantityObserver();
+        order.attach(priceObserver);
+        order.attach(quantityObserver);
+        order.addItem(50.0D);
+        System.out.println(order);
+        order.addItem(70.0D);
+        System.out.println(order);
+        order.addItem(80.0D);
+        System.out.println(order);
+        order.addItem(60.0D);
+        System.out.println(order);
+        order.addItem(30.0D);
+        System.out.println(order);
+        order.detach(quantityObserver);
+        order.addItem(40.0D);
+        System.out.println(order);
+    }
 }
--- src/QuantityObserver.java
+++ src/QuantityObserver.java
@@ -1,2 +1,26 @@
-public class QuantityObserver {
+public class QuantityObserver implements OrderObserver {
+    private static final double DEFAULT_SHIPPING_COST = 10;
+    private static final int MIN_ITEMS_FOR_FREE_SHIPPING = 5;
+    private boolean shippingCost = false;
+    private boolean freeShipping = false;
+
+    public QuantityObserver() {
+    }
+
+    public void update(Order order) {
+        double totalPrice = order.getTotalPrice();
+        if (order.getCount() < MIN_ITEMS_FOR_FREE_SHIPPING) {
+            if (!this.shippingCost) {
+                totalPrice += DEFAULT_SHIPPING_COST;
+                order.setTotalPrice(totalPrice);
+                System.out.println("Until you have 5 or more items, quantity monitor set the shipping cost to $10.");
+                this.shippingCost = true;
+            }
+        } else if (!this.freeShipping) {
+            totalPrice -= DEFAULT_SHIPPING_COST;
+            order.setTotalPrice(totalPrice);
+            System.out.println("Because you have 5 items or more, quantity monitor set the shipping cost to $0.");
+        }
+
+    }
 }
